cbd68447b3569fd33b1db679a1e472427fdbcd4e
zulfiqarshafar <zul.shafar@gmail.com>
2022-07-24 18:28:11 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Art Gallery`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
